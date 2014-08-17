e290358cbc2bb1bc6bb03a8ab7f5b61b943c30d3
RyuunoAelia <aelia@internux.ch>
2014-08-17 11:05:43 +0200

    Make a "simple" skeleton for the LPC2388 project. This already does much more than what we want.
    moreover the boot.s file tries to access reserved memory above the RAM
    but it simply works.

---
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 17, 2014
+ *      Author: Aelia
+ *
+ * This file was created for Ethack and is released under GPLv3.
+ * See the LICENSE file for more information about the GPLv3.
+ */
+
+int main(void)  __attribute__((noreturn));
+
+int
+main(void) {
+	while(1);
+}
+
+void vApplicationTickHook(void){
+}
