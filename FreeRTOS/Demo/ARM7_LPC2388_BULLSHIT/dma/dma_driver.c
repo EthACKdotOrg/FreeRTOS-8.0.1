e32b4924ccaf9ab9d5250f92675359c4069a31d4
RyuunoAelia <aelia@internux.ch>
2014-08-30 19:32:44 +0200

    Add the missing bits for the DMA driver that are used for the MMC/SD driver

--- FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/dma/dma_driver.c
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/dma/dma_driver.c
@@ -7,9 +7,33 @@
  * This file was created for Ethack and is released under GPLv3.
  * See the LICENSE file for more information about the GPLv3.
  */
+#include "stddef.h"
+#include "dma.h"
 
+static struct dma_registrations {
+	dma_callback callback;
+} registrations[DMA_CHANNEL_BOUND] = {0};
 
 void
 dma_init(void) {
 	// TODO
 }
+
+bool
+dma_start(enum dma_channel channel, enum dma_mode mode, uint32_t *buffer) {
+	return false;
+}
+
+enum dma_channel
+dma_reserve_channel(dma_callback callback) {
+	int i;
+	for (i = 0; i < DMA_CHANNEL_BOUND; i++) {
+		if (registrations[i].callback == NULL) {
+			registrations[i].callback = callback;
+			return i;
+		}
+	}
+	return DMA_CHANNEL_BOUND;
+}
+
+// TODO ISR that redirects to the registration callback
