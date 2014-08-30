d1db835ece76e9741a57c8213e89ee537974f3ab
RyuunoAelia <aelia@internux.ch>
2014-08-30 19:25:03 +0200

    Continue implementing the MMC/SD driver.

--- FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/mmc/mmc.h
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/mmc/mmc.h
@@ -20,60 +20,28 @@
  * 	MCIDAT3 out 71 (P2[13])
  */
 
+#define MMC_BLOCK_SIZE_POWER 9
+#define MMC_BLOCK_SIZE (2<<MMC_BLOCK_SIZE_POWER)
+#if MMC_BLOCK_SIZE_POWER > 11
+#error "MMC_BLOCK_SIZE_POWER cannot be greater than 11 (MMC_BLOCK_SIZE > 2048bit)"
+#endif
+#include "stdbool.h"
+#include "stdint.h"
+#include "FreeRTOS.h"
+#include "queue.h"
 
 #ifndef MMC_H_
 #define MMC_H_
 
+/*!
+ * These values are not correct!
+ * These values are dependent on the CPU speed and are computed for a CCLK of 24MHz.
+ */
 enum mmc_speed {
-	MMC_SPEED_400KHZ,
-	MMC_SPEED_24MHZ,
+	MMC_SPEED_400KHZ = 240,
+	MMC_SPEED_20MHZ  = 1,
+	MMC_SPEED_24MHZ  = 0,
 };
-
-enum mmc_command {
-	MMC_COMMAND_GO_IDLE_STATE       = 0 ,      /* Resetthe card to idle state  (MMC,SD) */
-	MMC_COMMAND_SEND_OP_COND        = 1 ,      /* Send Op.Cond. Register       (MMC)    */
-	MMC_COMMAND_ALL_SEND_CID        = 2 ,      /* Send Card CID number         (MMC,SD) */
-	MMC_COMMAND_SET_RELATIVE_ADDR   = 3 ,      /* Set Relative Address         (MMC,SD) */
-	MMC_COMMAND_SET_ACMD_BUS_WIDTH  = 6 ,
-	MMC_COMMAND_SELECT_CARD         = 7 ,      /* Select/Deselect the Card     (MMC,SD) */
-	MMC_COMMAND_SEND_CSD            = 9 ,      /* Send Card Specific Data      (MMC,SD) */
-	MMC_COMMAND_SEND_CID            = 10,      /* Send Card Identification Data(MMC,SD) */
-	MMC_COMMAND_STOP_TRANSMISSION   = 12,      /* Stop Read or Write transm.   (MMC,SD) */
-	MMC_COMMAND_SEND_STATUS         = 13,      /* Send Status Resiger          (MMC,SD) */
-	MMC_COMMAND_SET_BLOCK_LEN       = 16,      /* Set Block Length in bytes    (MMC,SD) */
-	MMC_COMMAND_READ_BLOCK          = 17,      /* Read a Single Block          (MMC,SD) */
-	MMC_COMMAND_READ_MULT_BLOCK     = 18,      /* Read Multiple Blocks         (MMC,SD) */
-	MMC_COMMAND_WRITE_BLOCK         = 24,      /* Write a Block                (MMC,SD) */
-	MMC_COMMAND_WRITE_MULT_BLOCK    = 25,      /* Write Multiple Blocks        (MMC,SD) */
-	MMC_COMMAND_SEND_APP_OP_COND    = 41,      /* Send App. Op.Cond Register       (SD) */
-	MMC_COMMAND_APP_CMD             = 55,      /* App.Specific Command follow  (MMC,SD) */
-};
-
-enum mmc_response_type {
-	MMC_RESPONSE_NONE,
-	MMC_RESPONSE_SHORT = 0x40,
-	MMC_RESPONSE_LONG = 0xC0,
-};
-#define MMC_COMMAND_FLAG   (0x400)
-
-#define MCI_INTERRUPT_LINE  (2<<24)
-#define MCI_PINSEL1_CLEAR   (~(1<< 6 | 1<< 8 | 1<<10 | 1<<12))
-#define MCI_PINSEL4_CLEAR   (~(1<<22 | 1<<24 | 1<<26))
-#define MCI_PINSEL1_P0_19   (1<< 7)
-#define MCI_PINSEL1_P0_20   (1<< 9)
-#define MCI_PINSEL1_P0_21   (1<<11)
-#define MCI_PINSEL1_P0_22   (1<<13)
-#define MCI_PINSEL1_SET     (MCI_PINSEL1_P0_19|MCI_PINSEL1_P0_20|MCI_PINSEL1_P0_21|MCI_PINSEL1_P0_22)
-#define MCI_PINSEL4_P2_11   (1<<23)
-#define MCI_PINSEL4_P2_12   (1<<25)
-#define MCI_PINSEL4_P2_13   (1<<27)
-#define MCI_PINSEL4_SET     (MCI_PINSEL4_P2_11|MCI_PINSEL4_P2_12|MCI_PINSEL4_P2_13)
-#define MCI_PCONP_ENABLE    (1<<28)
-#define MCI_PCLK_SEL1       (      1<<24)
-#define MCI_SCS_POWER       (1<<8)
-#define MCI_COMMAND_MASK    (0x3F)
-#define MCI_COMMAND_ACTIVE  (0x00000800)
-#define MCI_CLEAR_MASK      (0x7FF)
 
 enum mci_status {
 	MCI_STATUS_CMD_CRC_FAIL    = 0x00000001,
@@ -99,8 +67,130 @@ enum mci_status {
 	MCI_STATUS_TX_DATA_AVAIL   = 0x00100000,
 	MCI_STATUS_RX_DATA_AVAIL   = 0x00200000,
 };
+typedef void (*mmc_write_callback)(bool has_error);
+typedef void (*mmc_read_callback)(bool has_error, uint32_t *data);
 
+struct mmc_datawrite_request {
+	uint32_t block;
+	uint32_t data[MMC_BLOCK_SIZE / sizeof(uint32_t)];
+	mmc_write_callback callback;
+};
+
+struct mmc_dataread_request {
+	uint32_t block;
+	uint32_t data[MMC_BLOCK_SIZE / sizeof(uint32_t)];
+	mmc_read_callback callback;
+};
+
+enum mmc_command {
+	MMC_COMMAND_GO_IDLE_STATE       = 0 ,      /* Resetthe card to idle state  (MMC,SD) */
+	MMC_COMMAND_SEND_OP_COND        = 1 ,      /* Send Op.Cond. Register       (MMC)    */
+	MMC_COMMAND_ALL_SEND_CID        = 2 ,      /* Send Card CID number         (MMC,SD) */
+	MMC_COMMAND_SEND_RELATIVE_ADDR   = 3 ,      /* Set Relative Address         (MMC,SD) */
+	MMC_COMMAND_SET_ACMD_BUS_WIDTH  = 6 ,
+	MMC_COMMAND_SELECT_CARD         = 7 ,      /* Select/Deselect the Card     (MMC,SD) */
+	MMC_COMMAND_SEND_CSD            = 9 ,      /* Send Card Specific Data      (MMC,SD) */
+	MMC_COMMAND_SEND_CID            = 10,      /* Send Card Identification Data(MMC,SD) */
+	MMC_COMMAND_STOP_TRANSMISSION   = 12,      /* Stop Read or Write transm.   (MMC,SD) */
+	MMC_COMMAND_SEND_STATUS         = 13,      /* Send Status Resiger          (MMC,SD) */
+	MMC_COMMAND_SET_BLOCK_LEN       = 16,      /* Set Block Length in bytes    (MMC,SD) */
+	MMC_COMMAND_READ_BLOCK          = 17,      /* Read a Single Block          (MMC,SD) */
+	MMC_COMMAND_READ_MULT_BLOCK     = 18,      /* Read Multiple Blocks         (MMC,SD) */
+	MMC_COMMAND_WRITE_BLOCK         = 24,      /* Write a Block                (MMC,SD) */
+	MMC_COMMAND_WRITE_MULT_BLOCK    = 25,      /* Write Multiple Blocks        (MMC,SD) */
+	MMC_COMMAND_SEND_APP_OP_COND    = 41,      /* Send App. Op.Cond Register       (SD) */
+	MMC_COMMAND_APP_CMD             = 55,      /* App.Specific Command follow  (MMC,SD) */
+};
+
+enum mmc_response_type {
+	MMC_RESPONSE_NONE  =    0,
+	MMC_RESPONSE_SHORT = 0x40,
+	MMC_RESPONSE_LONG  = 0xC0,
+};
+
+#define MMC_COMMAND_TRANSMITTER_DIRECTION   (0x400)
+#define MCI_DATA_RD_TOUT_VALUE  (10*(24000000/1000))  /* ~10ms  at 24MHz SD clock */
+#define MCI_DATA_RW_TOUT_VALUE  (200*(24000000/1000)) /* ~200ms at 24MHz SD clock */
+
+#define SD_STATUS_ACMD_ENABLE            0x00000020
+#define SD_COMMAND_STATE_MASK            (SD_COMMAND_STATUS_READY_FOR_DATA|SD_COMMAND_STATUS_PRG|SD_COMMAND_STATUS_DIS) // 7 is 1 + 2 + 4, with 8 they span all possible values of the 4 bits of state
+#define SD_COMMAND_STATUS_READY_FOR_DATA (1<<8)
+#define SD_COMMAND_STATUS_IDLE           (0<<9)
+#define SD_COMMAND_STATUS_READY          (1<<9)
+#define SD_COMMAND_STATUS_IDENT          (2<<9)
+#define SD_COMMAND_STATUS_STBY           (3<<9)
+#define SD_COMMAND_STATUS_TRAN           (4<<9)
+#define SD_COMMAND_STATUS_DATA           (5<<9)
+#define SD_COMMAND_STATUS_RCV            (6<<9)
+#define SD_COMMAND_STATUS_PRG            (7<<9)
+#define SD_COMMAND_STATUS_DIS            (8<<9)
+
+#define SD_COMMAND_ARG_BUS_WIDTH_1BIT       0
+#define SD_COMMAND_ARG_BUS_WIDTH_4BITS     10
+
+#define SD_OCR_POWER_16DV_TO_17DV  (1<< 4)
+#define SD_OCR_POWER_17DV_TO_18DV  (1<< 5)
+#define SD_OCR_POWER_18DV_TO_19DV  (1<< 6)
+#define SD_OCR_POWER_19DV_TO_20DV  (1<< 7)
+#define SD_OCR_POWER_20DV_TO_21DV  (1<< 8)
+#define SD_OCR_POWER_21DV_TO_22DV  (1<< 9)
+#define SD_OCR_POWER_22DV_TO_23DV  (1<<10)
+#define SD_OCR_POWER_23DV_TO_24DV  (1<<11)
+#define SD_OCR_POWER_24DV_TO_25DV  (1<<12)
+#define SD_OCR_POWER_25DV_TO_26DV  (1<<13)
+#define SD_OCR_POWER_26DV_TO_27DV  (1<<14)
+#define SD_OCR_POWER_27DV_TO_28DV  (1<<15)
+#define SD_OCR_POWER_28DV_TO_29DV  (1<<16)
+#define SD_OCR_POWER_29DV_TO_30DV  (1<<17)
+#define SD_OCR_POWER_30DV_TO_31DV  (1<<18)
+#define SD_OCR_POWER_31DV_TO_32DV  (1<<19)
+#define SD_OCR_POWER_32DV_TO_33DV  (1<<20)
+#define SD_OCR_POWER_33DV_TO_34DV  (1<<21)
+#define SD_OCR_POWER_34DV_TO_35DV  (1<<22)
+#define SD_OCR_POWER_35DV_TO_36DV  (1<<23)
+#define SD_OCR_POWER_UP_STATUS     (1<<31)
+
+#define MCI_INTERRUPT_LINE  (2<<24)
+#define MCI_PINSEL1_CLEAR   (~(1<< 6 | 1<< 8 | 1<<10 | 1<<12))
+#define MCI_PINSEL4_CLEAR   (~(1<<22 | 1<<24 | 1<<26))
+#define MCI_PINSEL1_P0_19   (1<< 7)
+#define MCI_PINSEL1_P0_20   (1<< 9)
+#define MCI_PINSEL1_P0_21   (1<<11)
+#define MCI_PINSEL1_P0_22   (1<<13)
+#define MCI_PINSEL1_SET     (MCI_PINSEL1_P0_19|MCI_PINSEL1_P0_20|MCI_PINSEL1_P0_21|MCI_PINSEL1_P0_22)
+#define MCI_PINSEL4_P2_11   (1<<23)
+#define MCI_PINSEL4_P2_12   (1<<25)
+#define MCI_PINSEL4_P2_13   (1<<27)
+#define MCI_PINSEL4_SET     (MCI_PINSEL4_P2_11|MCI_PINSEL4_P2_12|MCI_PINSEL4_P2_13)
+#define MCI_PCONP_ENABLE    (1<<28)
+#define MCI_PCLK_SEL1       (      1<<24)
+#define MCI_SCS_POWER       (1<<8)
+#define MCI_COMMAND_MASK    (0x3F)
+#define MCI_COMMAND_ACTIVE  (0x00000800)
+#define MCI_CLEAR_MASK      (0x7FF)
+
+#define MCI_CONTROL_ENABLE         (1<<0)
+#define MCI_CONTROL_DIRECTION_READ (1<<1)
+#define MCI_CONTROL_MODE_STREAM    (1<<2)
+#define MCI_CONTROL_DMA_ENABLE     (1<<3)
+
+#define MCI_CLOCK_CONTROL_CLKDIV_MASK 0xFF
+#define MCI_CLOCK_CONTROL_ENABLE      (1<< 8)
+#define MCI_CLOCK_CONTROL_PWRSAVE     (1<< 9)
+#define MCI_CLOCK_CONTROL_BYPASS      (1<<10)
+#define MCI_CLOCK_CONTROL_WIDEBUS     (1<<11)
+
+#define MCI_POWER_CONTROL_CTRL_CLEAR_MASK (1<<0 | 1<<1)
+#define MCI_POWER_CONTROL_CTRL_POWER_OFF  (0)
+#define MCI_POWER_CONTROL_CTRL_POWER_UP   (1<<1)
+#define MCI_POWER_CONTROL_CTRL_POWER_ON   (1<<0 | 1<<1)
+#define MCI_POWER_CONTROL_OPENDRAIN       (1<<6)
+#define MCI_POWER_CONTROL_ROD             (1<<7)
+
+#define MCI_ARGUMENT_OCR_INDEX            0x00FF8000
 
 void mmc_init(int taskpriority);
+bool mmc_post_read_request(struct mmc_dataread_request *req);
+bool mmc_post_write_request(struct mmc_datawrite_request *req);
 
 #endif /* MMC_H_ */
--- FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/mmc/mmc_driver.c
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/mmc/mmc_driver.c
@@ -10,12 +10,38 @@
 
 #include "mmc.h"
 #include "lpc23xx.h"
-#include "stdint.h"
-#include "stdbool.h"
 #include "stddef.h"
-#include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
+#include "dma.h"
+
+#define MMC_TO_WRITE_QUEUE_LENGTH 5
+#define MMC_TO_READ_QUEUE_LENGTH  5
+#define MMC_STACK_SIZE 1024
+
+static TaskHandle_t mmcTask;
+static QueueHandle_t mmcToWriteQueue;
+static QueueHandle_t mmcToReadQueue;
+static QueueSetHandle_t mmcReadWriteSet;
+static uint32_t cardRCA = 0;
+static enum dma_channel dma_channel;
+static bool error = false;
+
+/*!
+ * Try to set the speed of the bus to the specified speed.
+ */
+static void
+mmc_set_speed(enum mmc_speed speed) {
+	   /* Set a MCI clock speed to desired value. */
+	   int i,clkdiv;
+	   MCI_CLOCK = (MCI_CLOCK &
+			   ~MCI_CLOCK_CONTROL_CLKDIV_MASK) |
+			   MCI_CLOCK_CONTROL_ENABLE |
+			   MCI_CLOCK_CONTROL_PWRSAVE |
+			   speed;
+	   /* To be sure that everything is perfectly stable, in theory it should be 3MCLK + 2PCLK */
+	   vTaskDelay(10 / portTICK_PERIOD_MS);
+}
 
 /*!
  * Send a command and check the response (if any).
@@ -26,7 +52,7 @@ mmc_send_command(enum mmc_command cmd, uint32_t arg, enum mmc_response_type resp
 	uint32_t cmd_data = cmd;
 	uint32_t status = 0;
 
-	cmd_data |= MMC_COMMAND_FLAG;
+	cmd_data |= MMC_COMMAND_TRANSMITTER_DIRECTION;
 	cmd_data |= resp_type;
    /* Send the command. */
 	MCI_ARGUMENT = arg;
@@ -83,6 +109,90 @@ mmc_send_command(enum mmc_command cmd, uint32_t arg, enum mmc_response_type resp
 	return true;
 }
 
+static bool
+mmc_detect_sd_card(void) {
+	int i;
+	uint32_t response[1];
+	MCI_POWER &= ~MCI_POWER_CONTROL_OPENDRAIN;
+	for (i = 0; i < 500; i++) {
+		if (mmc_send_command(MMC_COMMAND_APP_CMD,
+				0,
+				MMC_RESPONSE_SHORT,
+				response) && (response[0] & SD_STATUS_ACMD_ENABLE)) {
+			if (mmc_send_command(MMC_COMMAND_SEND_APP_OP_COND,
+						SD_OCR_POWER_32DV_TO_33DV|SD_OCR_POWER_33DV_TO_34DV,
+						MMC_RESPONSE_SHORT,
+						response) &&
+					(response[0] & SD_OCR_POWER_UP_STATUS)) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+static bool
+mmc_detect_mmc_card(void) {
+	uint32_t response[1];
+	int i;
+	MCI_POWER |= MCI_POWER_CONTROL_OPENDRAIN;
+	/* First try really hard to detect an MMC, send CMD1. */
+	for (i = 0; i < 100; i++) {
+		if (mmc_send_command(MMC_COMMAND_SEND_OP_COND, MCI_ARGUMENT_OCR_INDEX, MMC_RESPONSE_SHORT, response)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+static void
+mmc_read_cid(void) {
+	int i;
+	uint32_t response[4];
+	for (i = 0; i < 20; i++) {
+		if (mmc_send_command(MMC_COMMAND_ALL_SEND_CID, 0, MMC_RESPONSE_LONG, response)) {
+			// TODO write identifiers to debug
+		}
+	}
+}
+
+static bool
+mmc_set_address(bool has_mmc) {
+	/* Set Relative Address, send CMD3 after CMD2. */
+	int i;
+	uint32_t arg = has_mmc?0x00010000:0;
+	uint32_t response[1];
+	for (i = 0; i < 20; i++) {
+		if (mmc_send_command(MMC_COMMAND_SEND_RELATIVE_ADDR, arg, MMC_RESPONSE_SHORT, response)) {
+			if ((response[0] & SD_COMMAND_STATE_MASK) == SD_COMMAND_STATUS_IDENT|SD_COMMAND_STATUS_READY_FOR_DATA) {
+				cardRCA = response[0] >> 16; // save the new RCA to address the card in the future
+			}
+			if (has_mmc) {
+				cardRCA = arg>>16;
+			}
+			return true;
+		}
+	}
+	return false;
+}
+
+static bool
+mmc_select_card(void) {
+	/* Select the Card, send CMD7 after CMD9, inter-change state */
+	/* between STBY and TRANS after this command. */
+	int i;
+	uint32_t arg = cardRCA << 16;
+	uint32_t response[1];
+	for (i = 0; i < 200; i++) {
+		if (mmc_send_command(MMC_COMMAND_SELECT_CARD, arg, MMC_RESPONSE_SHORT, response)) {
+			if ((response[0] & MMC_COMMAND_SEND_RELATIVE_ADDR) == SD_COMMAND_STATUS_STBY|SD_COMMAND_STATUS_READY_FOR_DATA) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
 /*!
  * Try to detect the MMC/SD card and set the maximum working speed
  * \return true on success, false on error
@@ -90,29 +200,155 @@ mmc_send_command(enum mmc_command cmd, uint32_t arg, enum mmc_response_type resp
 static bool
 mmc_detect_card() {
 	int i;
+	uint32_t response[1];
+
+	bool has_mmc = false;
+	mmc_set_speed(MMC_SPEED_400KHZ);
 	/* the MMC bus should still be initializing from the hardware init */
-	vTaskDelay(portTICK_PERIOD_MS * 100);
+	vTaskDelay(100 / portTICK_PERIOD_MS);
 	/* Power on the Flash Card. */
-	MCI_POWER |= 0x01;
+	MCI_POWER |= MCI_POWER_CONTROL_CTRL_POWER_ON;
 	/* wait until the card is powered on */
-	vTaskDelay(portTICK_PERIOD_MS * 100);
+	vTaskDelay(100 / portTICK_PERIOD_MS);
 
 	/* Reset the card, send CMD0. */
 	mmc_send_command(MMC_COMMAND_GO_IDLE_STATE, 0, MMC_RESPONSE_NONE, NULL);
-	// TODO detect MMC or SD card plus its speed
+	if (mmc_detect_mmc_card()) {
+		has_mmc = true;
+	}
+	if (has_mmc || mmc_detect_sd_card()) {
+		mmc_read_cid();
+		if (!mmc_set_address(has_mmc)) {
+			return false;
+		}
+		if (has_mmc) {
+			MCI_POWER &= ~MCI_POWER_CONTROL_OPENDRAIN;
+		}
+		mmc_set_speed(MMC_SPEED_24MHZ); // try max speed immediately ?
+		if (!mmc_select_card()) {
+			return false;
+		}
+		if (!has_mmc) {
+			MCI_CLOCK |= MCI_CLOCK_CONTROL_WIDEBUS;
+			vTaskDelay(100 / portTICK_PERIOD_MS);
+			if (!mmc_send_command(MMC_COMMAND_SET_ACMD_BUS_WIDTH, SD_COMMAND_ARG_BUS_WIDTH_4BITS, MMC_RESPONSE_SHORT, response) ||
+					((response[0] & SD_COMMAND_STATE_MASK) != SD_COMMAND_STATUS_TRAN|SD_COMMAND_STATUS_READY_FOR_DATA)) {
+				MCI_CLOCK &= ~MCI_CLOCK_CONTROL_WIDEBUS;
+				vTaskDelay(100 / portTICK_PERIOD_MS);
+				if (!mmc_send_command(MMC_COMMAND_SET_ACMD_BUS_WIDTH, SD_COMMAND_ARG_BUS_WIDTH_1BIT, MMC_RESPONSE_SHORT, response)) {
+					return false;
+				}
+			}
+		}
+		for (i = 0; i < 20; i++) {
+			/* Send ACMD6 command to set the bus width. */
+			if (!mmc_send_command(MMC_COMMAND_SET_BLOCK_LEN, MMC_BLOCK_SIZE, MMC_RESPONSE_SHORT, response)) {
+				continue;
+			}
+			if ((response[0] & SD_COMMAND_STATE_MASK) == SD_COMMAND_STATUS_TRAN|SD_COMMAND_STATUS_READY_FOR_DATA) {
+				return true;
+			}
+		}
+	}
 	return false;
 }
 
-/*!
- * Try to set the speed of the bus to the specified speed.
- */
-static void
-mmc_set_speed(enum mmc_speed high_speed) {
+static bool
+mmc_wait_for_tran_state(void) {
+	// TODO
+	return false;
+}
+
+static bool
+mmc_cmd_read_block(uint32_t block, uint32_t count) {
+	// TODO
+	return false;
+}
+
+static bool
+mmc_cmd_write_block(uint32_t block, uint32_t count) {
+	// TODO
+	return false;
+}
+
+static bool
+mmc_write_sector(uint32_t sector, uint32_t *buffer, uint32_t cnt) {
+	int i;
+	uint32_t response[1];
+	if (!mmc_wait_for_tran_state()) {
+		/* Card not in TRAN state. */
+		return false;
+	}
+	/* Write a 512 byte sector to Flash Card. */
+	if (!mmc_wait_for_tran_state()) {
+		/* Card not in TRAN state. */
+		return false;
+	}
+	if (!mmc_cmd_write_block(sector, cnt)) {
+		/* Command Failed. */
+		return false;
+	}
+	for (i = 0; i < cnt; i++) {
+		MCI_DATA_TMR = MCI_DATA_RW_TOUT_VALUE;
+		MCI_DATA_LEN = MMC_BLOCK_SIZE;
+		dma_start(DMA_WRITE, dma_channel, buffer + i * MMC_BLOCK_SIZE);
+		MCI_DATA_CTRL = (MMC_BLOCK_SIZE_POWER << 4) | MCI_CONTROL_DMA_ENABLE | MCI_CONTROL_ENABLE;
+		vTaskSuspend(mmcTask);
+		while (MCI_STATUS != (MCI_STATUS_DATA_END | MCI_STATUS_DATA_BLK_END)) {
+			if (MCI_STATUS & (MCI_STATUS_DATA_CRC_FAIL | MCI_STATUS_DATA_TIMEOUT)) {
+				if (!mmc_send_command(MMC_COMMAND_STOP_TRANSMISSION, 0, MMC_RESPONSE_SHORT, response) ||
+						((response[0] & SD_COMMAND_STATE_MASK) != SD_COMMAND_STATUS_IDLE|SD_COMMAND_STATUS_READY_FOR_DATA)) {
+					return false;
+				}
+				/* If error while Data Block sending occured. */
+				if (!mmc_send_command(MMC_COMMAND_STOP_TRANSMISSION, 0, MMC_RESPONSE_SHORT, response) ||
+						((response[0] & SD_COMMAND_STATE_MASK) != SD_COMMAND_STATUS_IDLE|SD_COMMAND_STATUS_READY_FOR_DATA)) {
+					return false;
+				}
+				/* Write request Failed. */
+				return false;
+			}
+		}
+	}
+	if (!mmc_send_command(MMC_COMMAND_STOP_TRANSMISSION, 0, MMC_RESPONSE_SHORT, response) ||
+			((response[0] & SD_COMMAND_STATE_MASK) != SD_COMMAND_STATUS_IDLE|SD_COMMAND_STATUS_READY_FOR_DATA)) {
+		return false;
+	}
+	return true;
+}
+
+static bool
+mmc_read_sector(uint32_t sector, uint32_t *buffer, uint32_t cnt) {
+   /* Read one or more 512 byte sectors from Flash Card. */
+   int i;
+   if (!mmc_wait_for_tran_state()) {
+      /* Card not in TRAN state. */
+      return false;
+   }
+   if (!mmc_cmd_read_block(sector, cnt)) {
+      /* Command Failed. */
+      return false;
+   }
+   /* Set MCI Transfer registers. */
+   MCI_DATA_TMR  = MCI_DATA_RD_TOUT_VALUE;
+   MCI_DATA_LEN  = cnt * MMC_BLOCK_SIZE;
+
+   /* Start DMA Peripheral to Memory transfer. */
+   dma_start(dma_channel, DMA_READ, buffer);
+   MCI_DATA_CTRL = (MMC_BLOCK_SIZE_POWER << 4) | MCI_CONTROL_DMA_ENABLE | MCI_CONTROL_DIRECTION_READ | MCI_CONTROL_ENABLE;
+   dma_start(dma_channel, DMA_READ, buffer);
+   return true;
 }
 
 static
 portTASK_FUNCTION(mmc_driver_task, pvParameters) {
-	static bool initialized = false, error = false;
+	union {
+		struct mmc_datawrite_request write;
+		struct mmc_dataread_request read;
+	} data;
+	QueueSetMemberHandle_t requested_queue;
+	uint32_t response;
+	static bool initialized = false;
 	if (error) {
 		initialized = false;
 		// TODO debug write message about init retry due to error
@@ -125,7 +361,50 @@ portTASK_FUNCTION(mmc_driver_task, pvParameters) {
 		}
 	}
 	while (!error) {
-		// TODO read from incoming queue and write to MMC via DMA
+		requested_queue = xQueueSelectFromSet(mmcReadWriteSet, portMAX_DELAY);
+		if (requested_queue == mmcToWriteQueue) {
+			/* wait indefinitely for a message */
+			xQueueReceive(mmcToWriteQueue, &data.write, portMAX_DELAY);
+			if (!mmc_send_command(MMC_COMMAND_WRITE_BLOCK,
+					data.write.block * MMC_BLOCK_SIZE,
+					MMC_RESPONSE_SHORT,
+					&response)) {
+				error = true;
+				break;
+			}
+			if (!mmc_write_sector(data.write.block, data.write.data, 1)) {
+				error = true;
+			} else {
+				vTaskSuspend(mmcTask);
+			}
+			data.write.callback(error);
+			/* the write is finished now, switching to the next command */
+		}
+		if (requested_queue == mmcToReadQueue) {
+			xQueueReceive(mmcToReadQueue, &data.read, portMAX_DELAY);
+			if (!mmc_read_sector(data.read.block, data.read.data, 1)) {
+				error = true;
+			} else {
+				vTaskSuspend(mmcTask);
+			}
+			data.read.callback(error, data.read.data);
+			/*the read is finished now, switching to the next command */
+		}
+	}
+	/* on error exit the mmcTask */
+	vTaskDelete(mmcTask);
+}
+
+static void
+mmc_dma_callback(enum dma_state state) {
+	switch (state) {
+	case DMA_STATE_ERROR:
+	case DMA_STATE_TIMEOUT:
+		error = true;
+		/* no break */
+	case DMA_STATE_SUCCESS:
+		vTaskResume(mmcTask);
+		break;
 	}
 }
 
@@ -143,8 +422,30 @@ mmc_init(int taskpriority) {
 	MCI_DATA_CTRL = 0;
 	MCI_CLEAR     = 0x7FF;
 	/* Power up, switch on VCC for the Flash Card. */
-	MCI_POWER  = 0x02;
-	mmc_set_speed(MMC_SPEED_400KHZ);
-	xTaskCreate(mmc_driver_task, "MMC", configMINIMAL_STACK_SIZE, NULL, taskpriority, NULL);
+	MCI_POWER  = MCI_POWER_CONTROL_CTRL_POWER_UP;
+	xTaskCreate(mmc_driver_task, "MMC", MMC_STACK_SIZE, NULL, taskpriority, &mmcTask);
+	mmcToWriteQueue = xQueueCreate(4, sizeof(struct mmc_datawrite_request));
+	mmcToReadQueue  = xQueueCreate(4, sizeof(struct mmc_datawrite_request));
+	configASSERT(mmcTask);
+	configASSERT(mmcToReadQueue);
+	configASSERT(mmcToWriteQueue);
+	dma_channel = dma_reserve_channel(mmc_dma_callback);
+	mmcReadWriteSet = xQueueCreateSet(MMC_TO_WRITE_QUEUE_LENGTH + MMC_TO_READ_QUEUE_LENGTH);
+	configASSERT(mmcReadWriteSet);
 }
 
+bool
+mmc_post_read_request(struct mmc_dataread_request *req) {
+	if (xQueueSendToBack(mmcToReadQueue, req, portMAX_DELAY) != pdTRUE) {
+		return false;
+	}
+	return true;
+}
+
+bool
+mmc_post_write_request(struct mmc_datawrite_request *req) {
+	if (xQueueSendToBack(mmcToWriteQueue, req, portMAX_DELAY) != pdTRUE) {
+		return false;
+	}
+	return true;
+}
