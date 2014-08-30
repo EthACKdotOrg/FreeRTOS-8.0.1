46228edb17708b8430f4302a8204f0f66a2db185
RyuunoAelia <aelia@internux.ch>
2014-08-30 19:26:14 +0200

    Add the missing DMA bits that are used by the MMC/SD driver.

--- FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/dma/dma.h
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/dma/dma.h
@@ -10,7 +10,30 @@
 
 #ifndef DMA_H_
 #define DMA_H_
+#include "stdbool.h"
+#include "stdint.h"
+
+enum dma_mode {
+	DMA_READ,
+	DMA_WRITE,
+};
+
+enum dma_channel {
+	DMA_CHANNEL0,
+	DMA_CHANNEL1,
+	DMA_CHANNEL_BOUND,
+};
+
+enum dma_state {
+	DMA_STATE_SUCCESS,
+	DMA_STATE_TIMEOUT,
+	DMA_STATE_ERROR,
+};
+
+typedef void (*dma_callback)(enum dma_state);
 
 void dma_init(void);
+bool dma_start(enum dma_channel channel, enum dma_mode, uint32_t *buffer);
+enum dma_channel dma_reserve_channel(dma_callback callback);
 
 #endif /* DMA_H_ */
