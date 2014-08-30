4ab3de35cf968e967721f47f8ca4b9fddad8a566
RyuunoAelia <aelia@internux.ch>
2014-08-30 19:29:08 +0200

    Add some necessary API bits for the MMC/SD dirver queues.
    Also clean up some unneeded parts.

--- FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/FreeRTOSConfig.h
+++ FreeRTOS/Demo/ARM7_LPC2388_BULLSHIT/FreeRTOSConfig.h
@@ -81,22 +81,12 @@
  * See http://www.freertos.org/a00110.html.
  *----------------------------------------------------------*/
 
-/* Value to use on old rev '-' devices. */
-//#define configPINSEL2_VALUE	0x50151105
-
-/* Value to use on rev 'A' and newer devices. */
-#define configPINSEL2_VALUE 	0x50150105
-
-#ifndef configPINSEL2_VALUE
-	#error Please uncomment one of the two configPINSEL2_VALUE definitions above, depending on the revision of the LPC2000 device being used.
-#endif
-
 #define configUSE_PREEMPTION		1
 #define configUSE_IDLE_HOOK         0
 #define configUSE_TICK_HOOK         1
-#define configCPU_CLOCK_HZ          ( ( unsigned long ) 48000000 )	/* =12Mhz xtal multiplied by 5 using the PLL. */
+#define configCPU_CLOCK_HZ          ( ( unsigned long ) 24000000 )	/* =12Mhz xtal multiplied by 2 using the PLL. */
 #define configTICK_RATE_HZ          ( ( TickType_t ) 1000 )
-#define configMAX_PRIORITIES		( 4 )
+#define configMAX_PRIORITIES		( 10 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 104 )
 #define configTOTAL_HEAP_SIZE		( ( size_t ) ( 18 * 1024 ) )
 #define configMAX_TASK_NAME_LEN		( 10 )
@@ -104,6 +94,7 @@
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		1
 #define configUSE_MUTEXES			1
+#define configUSE_QUEUE_SETS        1
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES 		0
@@ -116,7 +107,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskPrioritySet            1
 #define INCLUDE_uxTaskPriorityGet           1
 #define INCLUDE_vTaskDelete                 1
-#define INCLUDE_vTaskCleanUpResources       0
+#define INCLUDE_vTaskCleanUpResources       1
 #define INCLUDE_vTaskSuspend                1
 #define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
@@ -130,31 +121,3 @@ FreeRTOS/Source/tasks.c for limitations. */
 
 
 #endif /* FREERTOS_CONFIG_H */
-
-
-#ifndef sbi
-#define sbi(x,y)	x|=(1 << (y))
-#endif
-
-#ifndef cbi 
-#define cbi(x,y)	x&=~(1 << (y))
-#endif
-
-#ifndef tstb
-#define tstb(x,y)	(x & (1 << (y)) ? 1 : 0)
-#endif
-
-#ifndef toggle 
-#define toggle(x,y)	x^=(1 << (y))
-#endif
-
-#ifndef BIT
-#define BIT(x)	(1 << (x))
-
-typedef struct
-{
-	long xColumn;
-	char *pcMessage;
-} xLCDMessage;
-
-#endif
